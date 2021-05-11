f77d04d6b18e4bd901413c082d08f2c40f54ae86
syaiful919 <syaiful919@gmail.com>
2021-05-11 10:37:38 +0700

    get book handler

--- src/handler.js
+++ src/handler.js
@@ -74,6 +74,34 @@ const addBookHandler = (request, h) => {
   return response;
 };
 
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const book = books.filter((n) => n.id === id)[0];
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
 };
--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,23 @@ const {
   deleteNoteByIdHandler,
 } = require('./notes_handler');
 
-const { addBookHandler } = require('./handler');
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/books',
     handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
   },
   {
     method: 'POST',
